9f07a72ee58f604a514b07a230e86f35823b4bf4
Tiffani A. Rengifo Mori <tiffanirengifo23@gmail.com>
2025-03-23 22:42:23 -0500

    updated

--- test_funciones.py
+++
@@ -1,8 +0,0 @@
-from app.utils.funciones import *
-
-# Prueba de funciones
-print(es_sano(80, False))  # True
-print(calcular_calorias([120, 200, 150]))  # 446.5
-print(calcular_costo({"nombre": "Fresa", "precio": 1200}, {"nombre": "Choco", "precio": 500}, {"nombre": "Maní", "precio": 900}))  # 2600
-print(calcular_rentabilidad(7500, {"nombre": "Fresa", "precio": 1200}, {"nombre": "Choco", "precio": 500}, {"nombre": "Maní", "precio": 900}))  # 4900
-print(producto_mas_rentable({"nombre": "A", "rentabilidad": 4000}, {"nombre": "B", "rentabilidad": 2500}, {"nombre": "C", "rentabilidad": 11000}, {"nombre": "D", "rentabilidad": 3200}))  # "C"
